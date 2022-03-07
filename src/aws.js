79a39b719a8bee03542295284af6b03243e55379
Ayodeji Fashola <ayodeji.fashola@casper.com>
2022-03-07 12:13:19 +0100

    add ebs block mapping feature

--- src/aws.js
+++ src/aws.js
@@ -47,6 +47,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    BlockDeviceMappings: config.input.blockDeviceMappings,
   };
 
   try {
